fadba4d8667990a3625ed099cc7b3f68e1e6d8d6
maryvito <maryv2@yandex.ru>
2024-05-24 15:02:49 +0300

    /

---
+++ src/main/java/com/dmdev/spring/database/repository/UserRepository.java
@@ -0,0 +1,58 @@
+package com.dmdev.spring.database.repository;
+
+import com.dmdev.spring.database.entity.Role;
+import com.dmdev.spring.database.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u " +
+        "where u.firstname like %:firstname% and u.lastname like %:lastname%")
+    List<User> findAllBy(String firstname, String lastname);
+
+    @Query(value = "SELECT u.* FROM users u WHERE u.username = :username",
+        nativeQuery = true)
+    List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update User u " +
+        "set u.role = :role " +
+        "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+    // Collection, Stream
+    // Streamable, Slice, Page
+//    @EntityGraph("User.company")
+    @EntityGraph(attributePaths = {"company", "company.locales"})
+    @Query(value = "select u from User u",
+           countQuery = "select count(distinct u.firstname) from User u")
+    Page<User> findAllBy(Pageable pageable);
+}
+
+
+
+
+
+
+
+
+
+
+
+
