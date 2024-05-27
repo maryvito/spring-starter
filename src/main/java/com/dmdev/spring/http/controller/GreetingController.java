552b3904781227a1a4a2b666e60d4ad015411e9d
maryvito <maryv2@yandex.ru>
2024-05-27 19:18:03 +0300

    .

---
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -0,0 +1,36 @@
+package com.dmdev.spring.http.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/api/v1")
+public class GreetingController {
+
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String JSESSIONID,
+                              @PathVariable("id") Integer id) {
+        String ageParamValue = request.getParameter("age");
+        String acceptHeader = request.getHeader("accept");
+        Cookie[] cookies = request.getCookies();
+
+        modelAndView.setViewName("greeting/hello");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/bye")
+    public ModelAndView bye() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("greeting/bye");
+
+        return modelAndView;
+    }
+}
