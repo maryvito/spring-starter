5dc867e2522c93c57d8137c73284ccbcba2d942e
maryvito <maryv2@yandex.ru>
2024-05-28 16:43:14 +0300

    /

---
+++ src/test/java/com/dmdev/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.dmdev.spring.integration.database.repository;
+
+import com.dmdev.spring.database.entity.Company;
+import com.dmdev.spring.database.repository.CompanyRepository;
+import com.dmdev.spring.integration.IntegrationTestBase;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.persistence.EntityManager;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RequiredArgsConstructor
+class CompanyRepositoryTest extends IntegrationTestBase {
+
+    private static final Integer APPLE_ID = 5;
+    private final EntityManager entityManager;
+    private final TransactionTemplate transactionTemplate;
+    private final CompanyRepository companyRepository;
+
+    @Test
+    void checkFindByQueries() {
+        companyRepository.findByName("google");
+        companyRepository.findAllByNameContainingIgnoreCase("a");
+    }
+
+    @Test
+    @Disabled
+    void delete() {
+        var maybeCompany = companyRepository.findById(APPLE_ID);
+        assertTrue(maybeCompany.isPresent());
+        maybeCompany.ifPresent(companyRepository::delete);
+        entityManager.flush();
+        assertTrue(companyRepository.findById(APPLE_ID).isEmpty());
+    }
+
+    @Test
+    void findById() {
+        transactionTemplate.executeWithoutResult(tx -> {
+            var company = entityManager.find(Company.class, 1);
+            assertNotNull(company);
+            assertThat(company.getLocales()).hasSize(2);
+        });
+    }
+
+    @Test
+    void save() {
+        var company = Company.builder()
+            .name("Apple1")
+            .locales(Map.of(
+                "ru", "Apple описание",
+                "en", "Apple description"
+            ))
+            .build();
+        entityManager.persist(company);
+        assertNotNull(company.getId());
+    }
+}
