4ef15ea44503d8c277e1edb5e521c805e02d3a14
maryvito <maryv2@yandex.ru>
2024-05-30 19:51:14 +0300

    /

---
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,40 @@
+package com.dmdev.spring.config;
+
+import com.dmdev.spring.database.pool.ConnectionPool;
+import com.dmdev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+
+//@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
+@Configuration(proxyBeanMethods = true)
+public class ApplicationConfiguration {
+
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 25);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
