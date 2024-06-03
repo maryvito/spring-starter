22231c1617d1f425bc1d107b6c6053b2a9ba5505
maryvito <maryv2@yandex.ru>
2024-06-03 18:42:29 +0300

    .

---
+++ src/test/java/com/dmdev/spring/integration/service/UserServiceIT.java
@@ -0,0 +1,104 @@
+package com.dmdev.spring.integration.service;
+
+import com.dmdev.spring.database.entity.Role;
+import com.dmdev.spring.dto.UserCreateEditDto;
+import com.dmdev.spring.dto.UserReadDto;
+import com.dmdev.spring.integration.IntegrationTestBase;
+import com.dmdev.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RequiredArgsConstructor
+public class UserServiceIT extends IntegrationTestBase {
+
+    private static final Long USER_1 = 1L;
+    private static final Integer COMPANY_1 = 1;
+
+    private final UserService userService;
+
+    @Test
+    void findAll() {
+        List<UserReadDto> result = userService.findAll();
+        assertThat(result).hasSize(5);
+    }
+
+    @Test
+    void findById() {
+        Optional<UserReadDto> maybeUser = userService.findById(USER_1);
+        assertTrue(maybeUser.isPresent());
+        maybeUser.ifPresent(user -> assertEquals("ivan@gmail.com", user.getUsername()));
+    }
+
+    @Test
+    void create() {
+        UserCreateEditDto userDto = new UserCreateEditDto(
+                "test@gmail.com",
+                "test",
+                LocalDate.now(),
+                "Test",
+                "Test",
+                Role.ADMIN,
+                COMPANY_1,
+                new MockMultipartFile("test", new byte[0])
+        );
+        UserReadDto actualResult = userService.create(userDto);
+
+        assertEquals(userDto.getUsername(), actualResult.getUsername());
+        assertEquals(userDto.getBirthDate(), actualResult.getBirthDate());
+        assertEquals(userDto.getFirstname(), actualResult.getFirstname());
+        assertEquals(userDto.getLastname(), actualResult.getLastname());
+        assertEquals(userDto.getCompanyId(), actualResult.getCompany().id());
+        assertSame(userDto.getRole(), actualResult.getRole());
+    }
+
+    @Test
+    void update() {
+        UserCreateEditDto userDto = new UserCreateEditDto(
+                "test@gmail.com",
+                "test",
+                LocalDate.now(),
+                "Test",
+                "Test",
+                Role.ADMIN,
+                COMPANY_1,
+                new MockMultipartFile("test", new byte[0])
+        );
+
+        Optional<UserReadDto> actualResult = userService.update(USER_1, userDto);
+
+        assertTrue(actualResult.isPresent());
+        actualResult.ifPresent(user -> {
+            assertEquals(userDto.getUsername(), user.getUsername());
+            assertEquals(userDto.getBirthDate(), user.getBirthDate());
+            assertEquals(userDto.getFirstname(), user.getFirstname());
+            assertEquals(userDto.getLastname(), user.getLastname());
+            assertEquals(userDto.getCompanyId(), user.getCompany().id());
+            assertSame(userDto.getRole(), user.getRole());
+        });
+    }
+
+    @Test
+    void delete() {
+        assertFalse(userService.delete(-124L));
+        assertTrue(userService.delete(USER_1));
+    }
+
+
+
+
+
+
+
+
+}
