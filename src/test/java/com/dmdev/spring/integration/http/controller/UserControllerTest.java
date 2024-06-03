954dc49b479deee60a9aed262ab53792a9481b96
maryvito <maryv2@yandex.ru>
2024-06-03 17:00:39 +0300

    .

---
+++ src/test/java/com/dmdev/spring/integration/http/controller/UserControllerTest.java
@@ -0,0 +1,68 @@
+package com.dmdev.spring.integration.http.controller;
+
+import com.dmdev.spring.database.entity.Role;
+import com.dmdev.spring.integration.IntegrationTestBase;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.dmdev.spring.dto.UserCreateEditDto.Fields.birthDate;
+import static com.dmdev.spring.dto.UserCreateEditDto.Fields.companyId;
+import static com.dmdev.spring.dto.UserCreateEditDto.Fields.firstname;
+import static com.dmdev.spring.dto.UserCreateEditDto.Fields.lastname;
+import static com.dmdev.spring.dto.UserCreateEditDto.Fields.role;
+import static com.dmdev.spring.dto.UserCreateEditDto.Fields.username;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+class UserControllerTest extends IntegrationTestBase {
+
+    private final MockMvc mockMvc;
+
+    @BeforeEach
+    void init() {
+//        List<GrantedAuthority> roles = Arrays.asList(Role.ADMIN, Role.USER);
+//        User testUser = new User("test@gmail.com", "test", roles);
+//        TestingAuthenticationToken authenticationToken = new TestingAuthenticationToken(testUser, testUser.getPassword(), roles);
+//
+//        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+//        securityContext.setAuthentication(authenticationToken);
+//        SecurityContextHolder.setContext(securityContext);
+    }
+
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(get("/users")
+                        .with(user("test@gmail.com").authorities(Role.ADMIN)))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("user/users"))
+                .andExpect(model().attributeExists("users"));
+    }
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(post("/users")
+                        .param(username, "test@gmail.com")
+                        .param(firstname, "Test")
+                        .param(lastname, "TestTest")
+                        .param(role, "ADMIN")
+                        .param(companyId, "1")
+                        .param(birthDate, "2000-01-01")
+                )
+                .andExpectAll(
+                        status().is3xxRedirection(),
+                        redirectedUrlPattern("/users/{\\d+}")
+                );
+    }
+
+
+}
